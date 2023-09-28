d335d97c327317284dce6c111bf16745ac82230e
DoganArslan1907 <Doganarslan3447@gmail.com>
2023-09-28 11:05:22 +0300

    LiveData and Hilt

---
+++ app/src/main/java/com/example/filmlerapp/data/repository/FilmlerRepository.kt
@@ -0,0 +1,11 @@
+package com.example.filmlerapp.data.repository
+
+import com.example.filmlerapp.data.datasource.FilmlerDataSource
+import com.example.filmlerapp.data.entity.Filmler
+
+
+class FilmlerRepository(var fds : FilmlerDataSource) {
+
+
+    suspend fun filmleriYukle() : List<Filmler> = fds.filmleriYukle()
+}
