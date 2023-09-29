11a80fccfedbd5905cd17c1740d68dc391d28363
DoganArslan1907 <Doganarslan3447@gmail.com>
2023-09-29 13:54:55 +0300

    Room Database Added.

--- app/src/main/java/com/example/filmlerapp/di/AppModule.kt
+++ app/src/main/java/com/example/filmlerapp/di/AppModule.kt
@@ -1,10 +1,17 @@
 package com.example.filmlerapp.di
 
+import android.content.Context
+import android.provider.ContactsContract.Data
+import androidx.room.Database
+import androidx.room.Room
 import com.example.filmlerapp.data.datasource.FilmlerDataSource
 import com.example.filmlerapp.data.repository.FilmlerRepository
+import com.example.filmlerapp.room.FilmlerDao
+import com.example.filmlerapp.room.Veritabani
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -13,8 +20,8 @@ import javax.inject.Singleton
 class AppModule {
     @Provides
     @Singleton
-    fun provideFilmlerDataSource() : FilmlerDataSource{
-        return FilmlerDataSource()
+    fun provideFilmlerDataSource(fDao : FilmlerDao) : FilmlerDataSource{
+        return FilmlerDataSource(fDao)
     }
 
     @Provides
@@ -22,4 +29,12 @@ class AppModule {
     fun provideFilmlerRepository(fds : FilmlerDataSource) : FilmlerRepository{
         return FilmlerRepository(fds)
     }
+    @Provides
+    @Singleton
+    fun provideFilmlerDao(@ApplicationContext context:Context) : FilmlerDao{
+        val vt = Room.databaseBuilder(context,Veritabani::class.java,"filmler_app.sqlite")
+            .createFromAsset("filmler_app.sqlite").build()
+        return vt.getFilmlerDao()
+    }
+
 }
